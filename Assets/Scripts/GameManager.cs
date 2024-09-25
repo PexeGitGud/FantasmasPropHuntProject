77d57a66606966a96770c7e430ae2b9ed2147c32
PexeGitGud <arthurmouraduarte.am@gmail.com>
2024-09-24 23:11:32 -0300

    Spawn Points

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,6 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class GameManager : MonoBehaviour
 {
@@ -18,34 +20,55 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     PlayerManager playerPrefab;
     [SerializeField]
+    Material outlineMaterial;
+
+    [SerializeField]
     GameObject classSelectionCamera;
     [SerializeField]
     GameObject classSelectionPanel;
+    [SerializeField]
+    GameObject playerUI;
+    [SerializeField]
+    TMP_Text playerClassText;
+
+    [SerializeField]
+    Color ghostColor;
+    [SerializeField]
+    Color hunterColor;
+
+    SpawnPoint[] spawnPoints;
 
     void Start()
     {
-        
-    }
+        playerUI.SetActive(false);
 
-    void Update()
-    {
-        
+        spawnPoints = FindObjectsOfType<SpawnPoint>();
     }
 
     public void SpawnGhost()
     {
-        PlayerManager spawnedPlayer = Instantiate(playerPrefab);
+        Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)].transform;
+        PlayerManager spawnedPlayer = Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
         spawnedPlayer.SelectClass(true);
         classSelectionCamera.SetActive(false);
         classSelectionPanel.SetActive(false);
+        playerUI.SetActive(true);
+        playerClassText.SetText("Ghost");
+        playerClassText.color = ghostColor;
+        outlineMaterial.SetColor("_Outline_Color", ghostColor);
     }
 
     public void SpawnHunter()
     {
-        PlayerManager spawnedPlayer = Instantiate(playerPrefab);
+        Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)].transform;
+        PlayerManager spawnedPlayer = Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
         spawnedPlayer.SelectClass(false);
         classSelectionCamera.SetActive(false);
         classSelectionPanel.SetActive(false);
+        playerUI.SetActive(true);
+        playerClassText.SetText("Hunter");
+        playerClassText.color = hunterColor;
+        outlineMaterial.SetColor("_Outline_Color", hunterColor);
     }
 
     public void ShowPlayerSelection()
@@ -53,5 +76,7 @@ public class GameManager : MonoBehaviour
         Cursor.lockState = CursorLockMode.None;
         classSelectionCamera.SetActive(true);
         classSelectionPanel.SetActive(true);
+        playerUI.SetActive(false);
+        playerClassText.SetText("");
     }
 }
